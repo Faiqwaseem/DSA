808ad0ad2b0a6e39556f80ebea8bfa0ea3981b6b
Muhammad Faiq <muhammadfaiqwaseem3@gmail.com>
2026-09-22 19:33:51 +0500

    solve secon smallest num

--- app.js
+++ app.js
@@ -562,4 +562,19 @@
 // }
 // console.log(find)
 
-const arr = [10, 5, 20, 8, 15];
+// Q20 — Second Smallest Number
+// const arr = [10, 5, 20, 8, 15];
+
+// let smallest = arr[0]
+// let secondSmallest = arr[0]
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] < smallest) {
+//         secondSmallest = smallest
+//        smallest = arr[i]
+//     }
+//     else if (arr[i] < secondSmallest) {
+//         secondSmallest = arr[i]
+//     }
+// }
+// console.log(secondSmallest, smallest)
+
